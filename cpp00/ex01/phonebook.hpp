c51b037827b5467d8b228042f4f327553fb96793
exceed96 <90549959+exceed96@users.noreply.github.com>
2022-10-18 17:32:58 +0900

    Add files via upload

---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangyeki <sangyeki@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 17:04:15 by sangyeki          #+#    #+#             */
+/*   Updated: 2022/10/18 14:51:54 by sangyeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _PHONEBOOK_HPP_
+#define _PHONEBOOK_HPP_
+#include "phonebook_info.hpp"
+
+class phonebook
+{
+	public:
+		phonebook(void);
+		void AddPhonebook(void);
+		void SearchPhonebook(void);
+	private:
+		int size;
+		Information informations[8];	
+};
+
+#endif
